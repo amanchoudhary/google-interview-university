c25b1868b1e73f7600eba6a05a045b49c2670cdf
amanchoudhary <void.aman93@gmail.com>
2017-12-06 08:37:03 +0530

    adding read stuff

--- cp/bst_tree.cpp
+++ cp/bst_tree.cpp
@@ -49,6 +49,72 @@ void inorder(struct node *head) {
 	inorder(head -> right);
 }
 
+void find_successor(struct node *head, int value, struct node *&successor, struct node *&predecessor) {
+	if (head == NULL) {
+		return;
+	}
+
+	if (head -> value == value) {
+		if (head -> right != NULL) {
+			struct node *temp = head -> right;
+			successor = temp;
+			while (temp -> left != NULL) {
+				temp = temp -> left;
+				successor = temp;
+			}
+		}
+
+		if (head -> left != NULL) {
+			struct node *temp = head -> left;
+			predecessor = temp;
+			while (temp -> right != NULL) {
+				temp = temp -> right;
+				predecessor = temp;
+			}
+		}
+
+		return;
+	}
+
+	if (value < head -> value) {
+		successor = head;
+		find_successor(head -> left, value, successor, predecessor);
+	} else {
+		predecessor = head;
+		find_successor(head -> right, value, successor, predecessor);
+	}
+}
+
+struct node* delete_node(struct node *head, int value) 
+{
+	if (value < head -> value) {
+		head -> left = delete_node(head -> left, value);
+		return head;
+	}
+	if (value > head -> value) {
+		head -> right = delete_node(head -> right, value);
+		return head;
+	}
+
+
+	if (head -> left == NULL && head -> right == NULL) {
+		return NULL;
+	}
+	if (head -> left == NULL) {
+		return head -> right;
+	}
+	if (head -> right == NULL) {
+		return head -> left;
+	}
+	struct node *temp = head -> right;
+	while (temp -> left != NULL) {
+		temp = temp -> left;
+	}
+	head -> value = temp -> value;
+	head -> right = delete_node(head -> right, temp -> value);
+	return head;
+}
+
 int main() {
 	struct node *head = NULL;
 	int a[10] = {32, 13, 23, 20, 30, 0, 1, 99, 10, 2};
@@ -63,4 +129,37 @@ int main() {
 
 	cout << "Is 20 present in BST: " << is_in_tree(head, 20) << endl;
 	cout << "Is 21 present in BST: " <<  is_in_tree(head, 21) << endl;
+
+	cout << " --------------------------------------------------- " << endl;
+
+	for (int i = 0; i < 10; i++) {
+		struct node *successor = NULL;
+		struct node *predecessor = NULL;
+		find_successor(head, a[i], successor, predecessor);
+		cout << "Successor of " << a[i] << " is : ";
+		if (successor != NULL) {
+			cout << successor -> value << endl;
+		} else {
+			cout << "-1" << endl;
+		}
+
+
+		cout << "Predecessor of " << a[i] << " is : ";
+		if (predecessor != NULL) {
+			cout << predecessor -> value << endl;
+		} else {
+			cout << "-1" << endl;
+		}
+	}
+
+	cout << " --------------------------------------------------- " << endl;
+
+	for (int i = 0; i < 10; i++) {
+		cout << "Deleting node value : " << a[i] << endl;
+		head = delete_node(head, a[i]);
+		inorder(head);
+		cout << endl;
+	}
+
+	return 0;
 }
